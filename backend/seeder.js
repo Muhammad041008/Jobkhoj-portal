b52a15dae5a16fb2ed8d22abd68d7b77025a7621
Muhammad041008 <mohdimran041008@gmail.com>
2025-09-28 00:41:37 +0530

    Update frontend: bug fixes and feature updates

--- backend/seeder.js
+++ backend/seeder.js
@@ -11,13 +11,11 @@ dotenv.config();
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('Connected to MongoDB');
-  seedDatabase();
-}).catch((err) => {
-  console.error('MongoDB connection error:', err);
-});
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
+
 
 // Sample data
 const sampleUsers = [
